a8725be6a6f4d45db2fd3c8f40028efe949f1228
Sergey26-zh <ivahnenkose.2003@gmail.com>
2024-01-11 13:02:41 +0700

    ready for work

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,7 @@
 package org.example;
 
-import lombok.AllArgsConstructor;
 
 public class Main {
     public static void main(String[] args) {
-Animal animal = new Animal(2);
-        System.out.println(animal.getAge());
     }
 }
